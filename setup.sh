f0f71da130ffb028f094d9696e10ef84bf7e9358
Andrew Vitale <vitale232@gmail.com>
2022-07-12 17:03:31 -0400

    fix: add stow

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ fi
 # Update the debian variant and get some useful programs
 sudo apt-get update && sudo apt-get dist-upgrade -y
 
-sudo apt-get install -y git pass gpg tree vim htop curl wget gimp
+sudo apt-get install -y stow git pass gpg tree vim htop curl wget gimp
 
 # Rust or bust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
