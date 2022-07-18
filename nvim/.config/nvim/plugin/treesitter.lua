565e4a51769911fa1a7acb8ef5e3987a8227fa5f
Andrew Vitale <vitale232@gmail.com>
2022-07-18 08:31:42 -0400

    feat: add lua and python to treesitter conf

--- nvim/.config/nvim/plugin/treesitter.lua
+++ nvim/.config/nvim/plugin/treesitter.lua
@@ -1,7 +1,15 @@
-require'nvim-treesitter.configs'.setup {
-	ensure_installed = {"typescript", "angular", "html", "css", "rust"},
+require("nvim-treesitter.configs").setup({
+	ensure_installed = {
+		"angular",
+		"css",
+		"html",
+		"lua",
+		"python",
+		"rust",
+		"typescript",
+	},
 	sync_install = false,
 	highlight = {
 		enable = true,
-	}
-}
+	},
+})
