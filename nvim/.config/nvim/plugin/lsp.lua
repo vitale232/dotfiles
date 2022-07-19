7ba1c7b0e333086c54002ae7f58c25afbc73d612
Andrew Vitale <vitale232@gmail.com>
2022-07-19 08:58:34 -0400

    chore: making moves

--- nvim/.config/nvim/plugin/lsp.lua
+++ nvim/.config/nvim/plugin/lsp.lua
@@ -2,6 +2,16 @@ local lspconfig = require("lspconfig")
 local null_ls = require("null-ls")
 local eslint = require("eslint")
 
+null_ls.setup({
+	sources = {
+		-- disable eslint (next 2 lines) for pa-share
+		null_ls.builtins.diagnostics.eslint,
+		null_ls.builtins.code_actions.eslint,
+		null_ls.builtins.formatting.prettier,
+		null_ls.builtins.formatting.stylua,
+	},
+})
+
 lspconfig.pyright.setup({})
 
 lspconfig.sumneko_lua.setup({
@@ -75,15 +85,6 @@ lspconfig.tsserver.setup({
 
 		on_attach(client, bufnr)
 	end,
-})
-
-null_ls.setup({
-	sources = {
-		null_ls.builtins.diagnostics.eslint,
-		null_ls.builtins.code_actions.eslint,
-		null_ls.builtins.formatting.prettier,
-		null_ls.builtins.formatting.stylua,
-	},
 })
 
 eslint.setup({
